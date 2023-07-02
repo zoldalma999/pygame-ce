e6074eff3103b385242c5b7e38b6005e8b245d87
zoldalma <46655437+zoldalma999@users.noreply.github.com>
2023-07-02 16:36:44 +0200

    Move doc headers and generate them correctly

--- buildconfig/makeref.py
+++ buildconfig/makeref.py
@@ -3,12 +3,44 @@
 import sys
 import os
 import subprocess
+import glob
 
 rst_dir = 'docs'
 rst_source_dir = os.path.join(rst_dir, 'reST')
 rst_build_dir = os.path.join('docs', 'generated')
 rst_doctree_dir = os.path.join(rst_build_dir, 'doctrees')
-c_header_dir = os.path.join('src_c', 'doc')
+c_header_dir = os.path.join(rst_build_dir, 'c_headers')
+
+special_paths = {
+    "music": os.path.join("src", "mixer"),
+    "pygame": os.path.join("src", "base")
+}
+
+def move_doc_headers():
+    # will not find (and is fine): examples, cdrom, overlay, tests
+    for file in glob.glob(f"{c_header_dir}/*.h"):
+        is_sdl2 = False
+        module_name = file.replace("_doc.h", "").replace(c_header_dir + os.sep, "")
+
+        if "sdl2_" in module_name:
+            is_sdl2 = True
+            module_name = module_name.replace("sdl2_", "")
+
+        args = ["src", "_sdl2", module_name]
+        if not is_sdl2:
+            args.pop(1)
+
+        path = os.path.join(*args)
+        file_name = f"{module_name}_doc.h"
+
+        if module_name in special_paths:
+            path = special_paths[module_name]
+
+        if os.path.exists(path):
+            os.replace(file, os.path.join(path, file_name))
+            print(f"Moved file {file_name} from {file} to {os.path.join(path, file_name)}")
+        else:
+            print(f"Skipping file {file} because there is no destination")
 
 
 def run():
@@ -21,13 +53,18 @@ def run():
                            '-b', 'html',
                            '-d', rst_doctree_dir,
                            '-D', f'headers_dest={c_header_dir}',
-                           '-D', 'headers_mkdirs=0',
+                           '-D', 'headers_mkdirs=1',
                            rst_source_dir,
                            rst_build_dir, ]
         if full_generation_flag:
             subprocess_args.append('-E')
         print("Executing sphinx in subprocess with args:", subprocess_args)
-        return subprocess.run(subprocess_args).returncode
+        retcode = subprocess.run(subprocess_args).returncode
+        if retcode != 0:
+            return retcode
+
+        move_doc_headers()
+
     except Exception:
         print('---')
         print('Have you installed sphinx?')
--- src_c/doc/tests_doc.h
+++
@@ -1,3 +0,0 @@
-/* Auto generated file: with makeref.py .  Docs go in docs/reST/ref/ . */
-#define DOC_TESTS "Pygame unit test suite package"
-#define DOC_TESTS_RUN "run(*args, **kwds) -> tuple\nRun the pygame unit test suite"
